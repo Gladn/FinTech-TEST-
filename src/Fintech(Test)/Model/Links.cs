d397e4d00902cac7da82eaf7a6b9196a572ff798
Alexander Gladkov <gladkov481@gmail.com>
2023-12-07 17:53:59 +0700

    Project created
    Каркас приложения

---
+++ src/Fintech(Test)/Model/Links.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Fintech_Test_.Model
+{
+    public class Links
+    {
+        public long UpProductId { get; set; } 
+        public long ProductId { get; set; }    
+        public int Count { get; set; }
+    }
+}
---
+++ src/Fintech(Test)/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Fintech_Test_.Model
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }    
+        public Decimal Price { get; set; }
+    }
+}
