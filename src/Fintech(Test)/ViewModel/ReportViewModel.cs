7a3876558cec3523321dd847c65604f3f933a527
Alexander Gladkov <gladkov481@gmail.com>
2023-12-11 02:36:45 +0700

    Report excel command
    Написана команда для вывода в excel

--- src/Fintech(Test)/ViewModel/ReportViewModel.cs
+++ src/Fintech(Test)/ViewModel/ReportViewModel.cs
@@ -1,26 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OfficeOpenXml;
-using System.Data.SqlClient;
-using System.IO;
-using System.Windows.Input;
-using Microsoft.Win32;
-using Fintech_Test_.ViewModel.Command;
-using System.Data;
-using Fintech_Test_.Model;
-using System.Collections.ObjectModel;
+﻿using Fintech_Test_.Model;
 using Fintech_Test_.Model.DataContext;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using Fintech_Test_.ViewModel.Command;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+
 
 namespace Fintech_Test_.ViewModel
 {
     class ReportViewModel : ViewModelBase
     {
-        private ObservableCollection<Report> _dataReport;        
+        private ObservableCollection<Report> _dataReport;
         public ObservableCollection<Report> DataReport
         {
             get => _dataReport;
@@ -29,7 +22,6 @@ namespace Fintech_Test_.ViewModel
 
 
         public ICommand LoadReportDataCommand { get; }
-        private bool CanLoadReportDataCommandExecute(object parameter) => true;
         private async void OnLoadReportDataCommandExecuted(object parameter)
         {
             await LoadReportDataAsync();
@@ -37,105 +29,110 @@ namespace Fintech_Test_.ViewModel
 
         public async Task LoadReportDataAsync()
         {
-            using (var context = new ApplicationContext())
+            try
             {
-                DataReport = new ObservableCollection<Report>(await context.ReportForProductLinks.FromSqlRaw("EXEC ReportForProductLinks").ToListAsync());
+                using (var context = new ApplicationContext())
+                {
+                    DataReport = new ObservableCollection<Report>(await context.ReportForProductLinks.FromSqlRaw("EXEC ReportForProductLinks").ToListAsync());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
 
-        //private void LoadData()
-        //{
-        //    using (var context = new YourDbContext())
-        //    {
-        //        var sql = "EXEC GetMyData";
-        //        var adapter = new SqlDataAdapter(sql, context.Database.Connection.ConnectionString);
-        //        var dataSet = new DataSet();
-        //        adapter.Fill(dataSet);
+        public ICommand LoadReportDataExcelCommand { get; }
+        private async void OnLoadReportDataExcelCommandExecuted(object parameter)
+        {
+            await LoadReportDataExcelAsync();
+        }
 
-        //        DataTable = dataSet.Tables.Count > 0 ? dataSet.Tables[0] : null;
-        //    }
-        //}
+        public async Task LoadReportDataExcelAsync()
+        {
+            try
+            {
+                using (ExcelPackage package = new ExcelPackage())
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Report");
 
+                    ExcelColumn column2 = worksheet.Column(2); column2.Width = 25;
+                    ExcelColumn column3 = worksheet.Column(3); column3.Width = 15;
+                    ExcelColumn column4 = worksheet.Column(4); column4.Width = 10;
+                    ExcelColumn column5 = worksheet.Column(5); column5.Width = 15;
+                    ExcelColumn column6 = worksheet.Column(6); column6.Width = 20;
 
-
-
-        //private void LoadData()
-        //{
-        //    using (var context = new YourDbContext()) // Замените YourDbContext на ваш контекст базы данных
-        //    {
-        //        var dataTable = new DataTable();
-        //        using (var command = context.Database.Connection.CreateCommand())
-        //        {
-        //            command.CommandText = "EXEC GetMyData";
-        //            context.Database.Connection.Open();
-        //            using (var reader = command.ExecuteReader())
-        //            {
-        //                dataTable.Load(reader);
-        //            }
-        //        }
-
-        //        Data = dataTable;
-        //    }
-        //}
+                    worksheet.Cells[2, 2].Value = "Название";
+                    worksheet.Cells[2, 3].Value = "Количество";
+                    worksheet.Cells[2, 4].Value = "Цена";
+                    worksheet.Cells[2, 5].Value = "Стоимость";
+                    worksheet.Cells[2, 6].Value = "Кол-во входящих";
 
 
+                    for (int i = 0; i < DataReport.Count; i++)
+                    {
+                        worksheet.Cells[i + 3, 2].Value = DataReport[i].Name;
+                        worksheet.Cells[i + 3, 3].Value = DataReport[i].Count;
+                        worksheet.Cells[i + 3, 4].Value = DataReport[i].Price;
+                        worksheet.Cells[i + 3, 5].Value = DataReport[i].AmountChildTotalCost;
+                        worksheet.Cells[i + 3, 6].Value = DataReport[i].AmountChildCount;
+                    }
+
+                    using (var range = worksheet.Cells[2, 2, 2, 20])
+                    {
+                        range.Style.Font.Size = 14;
+                    }
+
+                    for (int i = 1; i <= DataReport.Count + 2; i++)
+                    {
+                        using (var range = worksheet.Cells[i, 2, i, 20])
+                        {
+                            range.Style.Font.Size = 14;
+                        }
+                    }
 
 
-        //private void ExportToExcel()
-        //{
-        //    using (var package = new ExcelPackage())
-        //    {
-        //        var worksheet = package.Workbook.Worksheets.Add("Data");
+                    var saveFileDialog = new Microsoft.Win32.SaveFileDialog
+                    {
+                        Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*",
+                        FileName = "Report.xlsx"
+                    };
 
-        //        // Получаем данные из базы данных
-        //        using (var connection = new SqlConnection("your_connection_string"))
-        //        {
-        //            connection.Open();
-        //            using (var command = new SqlCommand("SELECT * FROM YourTableName", connection))
-        //            using (var reader = command.ExecuteReader())
-        //            {
-        //                int col = 1;
-        //                for (int i = 0; i < reader.FieldCount; i++)
-        //                {
-        //                    worksheet.Cells[1, col].Value = reader.GetName(i);
-        //                    col++;
-        //                }
 
-        //                int row = 2;
-        //                while (reader.Read())
-        //                {
-        //                    col = 1;
-        //                    for (int i = 0; i < reader.FieldCount; i++)
-        //                    {
-        //                        worksheet.Cells[row, col].Value = reader[i];
-        //                        col++;
-        //                    }
-        //                    row++;
-        //                }
-        //            }
-        //        }
+                    if (saveFileDialog.ShowDialog() == true)
+                    {
+                        var fileInfo = new System.IO.FileInfo(saveFileDialog.FileName);
+                        package.SaveAs(fileInfo);
+                        try
+                        {
+                            Process.Start(new ProcessStartInfo
+                            {
+                                FileName = fileInfo.FullName,
+                                UseShellExecute = true
+                            });
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Ошибка при открытии файла: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
 
-        //        // Сохраняем файл Excel
-        //        var saveFileDialog = new SaveFileDialog
-        //        {
-        //            Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*",
-        //            FileName = "ExportedData.xlsx"
-        //        };
-
-        //        if (saveFileDialog.ShowDialog() == DialogResult.OK)
-        //        {
-        //            var excelFile = new FileInfo(saveFileDialog.FileName);
-        //            package.SaveAs(excelFile);
-        //        }
-        //    }
-        //}
 
         public ReportViewModel()
         {
             _ = LoadReportDataAsync();
 
             LoadReportDataCommand = new RelayCommand(OnLoadReportDataCommandExecuted);
-        } 
+
+            LoadReportDataExcelCommand = new RelayCommand(OnLoadReportDataExcelCommandExecuted);
+        }
     }
 }
