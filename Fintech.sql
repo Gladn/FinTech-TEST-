18eb4a5631610be60829f38f69f2e96993763e4f
Alexander Gladkov <gladkov481@gmail.com>
2023-12-10 21:22:38 +0700

    Update Fintech.sql
    Доделан запрос для вкладки 3
    Осталось все привести более к читабельную варианту

--- Fintech.sql
+++ Fintech.sql
@@ -8,13 +8,23 @@ Name varchar(100),
 Price decimal(20,2))
 
 Create table Links (
+LinkId bigint primary key identity,
 UpProductId bigint,
 ProductId bigint,
 Count int,
-NodeHierarchy HIERARCHYID,
 Foreign key (UpProductId) references Product(Id),
-Foreign key (ProductId) references Product(Id),
-CONSTRAINT UQ_Links_UpProductId_ProductId UNIQUE (UpProductId, ProductId))
+Foreign key (ProductId) references Product(Id))
+
+--------------------------------------------------------------------------
+--Create table Links (
+--UpProductId bigint,
+--ProductId bigint,
+--Count int,
+--NodeHierarchy HIERARCHYID,
+--Foreign key (UpProductId) references Product(Id),
+--Foreign key (ProductId) references Product(Id),
+--CONSTRAINT UQ_Links_UpProductId_ProductId UNIQUE (UpProductId, ProductId))
+--------------------------------------------------------------------------
 
 
 INSERT INTO Product (Name, Price)
@@ -26,21 +36,9 @@ VALUES
 ('Изделие 5', 300),
 ('Изделие 6', 20),
 ('Изделие 7', 1000),
-('Изделие 8', 100);
-
-INSERT INTO Links (UpProductId, ProductId, Count, NodeHierarchy)
-VALUES 
-(null, 1, 1, HIERARCHYID::GetRoot()),
-(1, 2, 10,HIERARCHYID::Parse('/1/')),
-(1, 3, 2,HIERARCHYID::Parse('/2/')),
-(3, 5, 2,HIERARCHYID::Parse('/2/1/')),
-(1, 4, 1,HIERARCHYID::Parse('/3/')),
-(4, 2, 1,HIERARCHYID::Parse('/4/2/')),
-(4, 6, 5,HIERARCHYID::Parse('/4/6/')) ,
-(null, 7, 1, HIERARCHYID::GetRoot()),
-(7, 8, 20),
-(7, 3, 10),
-(3,2,10);
+('Изделие 8', 100),
+('Изделие 9', 1000),
+('Изделие 10', 500);
 
 INSERT INTO Links (UpProductId, ProductId, Count)
 VALUES 
@@ -49,12 +47,12 @@ VALUES
 (1, 3, 2),
 (3, 5, 2),
 (1, 4, 1),
-(4, 2, 1),
-(4, 6, 5) ,
-(null, 7, 1),
-(7, 8, 20),
-(7, 3, 10),
-(3,2,10);
+(4, 6, 1),
+(4, 7, 5) ,
+(null, 8, 1),
+(8, 9, 10),
+(9, 10, 5);
+
 
 select * from Product
 select * from Links
@@ -64,8 +62,356 @@ drop table Links
 drop table Product
 
 
-select * from Links as L
+select UpProductId, C.Name, ProductId, P.Name, Count, P.Price from Links as L
 join Product as P on P.Id = L.ProductId
+left join Product as c on C.Id = L.UpProductId
+
+
+
+--------------Версия Price-----------------------------------------------------
+Create table #TempTable (
+UpProductId bigint,
+ProductId bigint,
+Name varchar(100),
+Count int,
+Price dec(18,2),
+AmountChild int
+
+);
+
+
+;With RecursiveCTE as (  
+  select L.ProductId,  
+         L.Count,  
+		 P.Price,
+         L.ProductId as RootID
+  from Links L
+    join Product P on L.ProductId = P.Id
+  
+  union all  
+  
+  select L.ProductId,  
+         L.Count,   
+		 P.Price,
+         R.RootID
+  from Links L
+    join Product P on L.ProductId = P.Id
+    join RecursiveCTE R on L.UpProductId = R.ProductId  
+)  
+INSERT INTO #TempTable (UpProductId, ProductId, Name, Count, Price, AmountChild)
+SELECT L.UpProductId AS UpProductId,   
+       L.ProductId AS ProductId,   
+       P.Name,  
+       L.Count,  
+	   P.Price,
+       ISNULL(R.AmountChild, 0) AS AmountChild
+
+FROM Links L
+JOIN Product P ON L.ProductId = P.Id
+LEFT JOIN (  
+             SELECT RootID,  
+                    SUM(Price*Count) AS AmountChild  
+             FROM RecursiveCTE
+             GROUP BY RootID  
+             ) AS R  ON P.Id = R.RootID
+option (maxrecursion 0);
+
+SELECT * FROM  #TempTable;
+DROP TABLE #TempTable;
+
+
+
+
+
+
+-----------------Версия Count
+Create table #TempTable (
+UpProductId bigint,
+ProductId bigint,
+Name varchar(100),
+Count int,
+Price dec(18,2),
+AmountChild int
+
+);
+
+
+;With RecursiveCTE as (  
+  select L.ProductId,  
+         L.Count,  
+		 P.Price,
+         L.UpProductId as RootID
+  from Links L
+    join Product P on L.ProductId = P.Id
+  
+  union all  
+  
+  select L.ProductId,  
+         L.Count,   
+		 P.Price,
+         R.RootID
+  from Links L
+    join Product P on L.ProductId = P.Id
+    join RecursiveCTE R on L.UpProductId = R.ProductId  
+)  
+INSERT INTO #TempTable (UpProductId, ProductId, Name, Count, Price, AmountChild)
+SELECT L.UpProductId AS UpProductId,   
+       L.ProductId AS ProductId,   
+       P.Name,  
+       L.Count,  
+	   P.Price,
+       ISNULL(R.AmountChild, 0) AS AmountChild
+
+FROM Links L
+JOIN Product P ON L.ProductId = P.Id
+LEFT JOIN (  
+             SELECT RootID,  
+                    SUM(Count) AS AmountChild  
+             FROM RecursiveCTE
+             GROUP BY RootID  
+             ) AS R  ON P.Id = R.RootID
+
+--Order by P.Id  
+--option (maxrecursion 0);
+
+SELECT * FROM  #TempTable;
+DROP TABLE #TempTable;
+----------ВАРИАНТ 111--------------------------------------------
+CREATE TABLE #TempTable (
+    UpProductId BIGINT,
+    ProductId BIGINT,
+    Name VARCHAR(100),
+    Count INT,
+    Price DEC(20,2),
+    AmountChildTotalCost DEC(20,2),
+	AmountChildCount Int,
+    Level INT
+);
+
+WITH RecursiveCTE AS (
+    SELECT
+        L.ProductId,
+        L.Count,
+        P.Price,
+        0 AS Level,
+        L.ProductId AS RootID,
+		L.UpProductId as RootID2
+    FROM
+        Links L
+        JOIN Product P ON L.ProductId = P.Id
+
+    UNION ALL
+
+    SELECT
+        L.ProductId,
+        L.Count,
+        P.Price,
+        R.Level + 1,
+        R.RootID,
+		R.RootID2
+    FROM
+        Links L
+        JOIN Product P ON L.ProductId = P.Id
+        JOIN RecursiveCTE R ON L.UpProductId = R.ProductId
+)
+INSERT INTO #TempTable (UpProductId, ProductId, Name, Count, Price, AmountChildTotalCost, AmountChildCount,Level)
+SELECT
+    L.UpProductId AS UpProductId,
+    L.ProductId AS ProductId,
+    (
+        SELECT TOP 1 REPLICATE('  ', R.Level) + P.Name
+        FROM RecursiveCTE R
+        WHERE P.Id = R.ProductId
+        ORDER BY R.Level DESC -- Order by Level in descending order to get the topmost indentation
+    ) AS Name,
+    L.Count,
+    P.Price,
+    ISNULL(R.AmountChildTotalCost, 0) AS AmountChildTotalCost,
+	ISNULL(R2.AmountChildCount, 0) AS AmountChildCount,
+    R.Level
+
+FROM
+    Links L
+    JOIN Product P ON L.ProductId = P.Id
+    LEFT JOIN (
+        SELECT
+            RootID, 
+            SUM(Price*Count) AS AmountChildTotalCost,
+            MAX(Level) AS Level
+        FROM
+            RecursiveCTE
+        GROUP BY
+            RootID
+    ) AS R ON P.Id = R.RootID
+	    LEFT JOIN (
+        SELECT
+            RootID2,
+            SUM(Count) AS AmountChildCount,
+            MAX(Level) AS Level
+        FROM
+            RecursiveCTE
+        GROUP BY
+            RootID2
+    ) AS R2 ON P.Id = R2.RootID2;
+-- Select and view the contents of the temporary table
+SELECT * FROM #TempTable;
+
+-- Drop the temporary table
+DROP TABLE #TempTable;
+-------------ВАРИАН2-----------------------------------------
+CREATE TABLE #TempTable (
+    UpProductId BIGINT,
+    ProductId BIGINT,
+    Name VARCHAR(100),
+    Count INT,
+    Price DEC(20,2),
+    AmountChildTotalCost DEC(20,2),
+	AmountChildCount Int,
+    Level INT
+);
+
+WITH RecursiveCTE AS (
+    SELECT
+        L.ProductId,
+        L.Count,
+        P.Price,
+        0 AS Level,
+        L.ProductId AS RootID,
+		L.UpProductId as RootID2
+    FROM
+        Links L
+        JOIN Product P ON L.ProductId = P.Id
+		Left JOIN Product C ON L.UpProductId = C.Id
+
+    UNION ALL
+
+    SELECT
+        L.ProductId,
+        L.Count,
+        P.Price,
+        R.Level + 1,
+        R.RootID,
+		R.RootID2
+    FROM
+        Links L
+        JOIN Product P ON L.ProductId = P.Id
+        JOIN RecursiveCTE R ON L.UpProductId = R.ProductId
+)
+INSERT INTO #TempTable (UpProductId, ProductId, Name, Count, Price, AmountChildTotalCost, AmountChildCount,Level)
+SELECT
+    L.UpProductId AS UpProductId,
+    L.ProductId AS ProductId,
+    (
+        SELECT TOP 1 REPLICATE('  ', R.Level) + P.Name
+        FROM RecursiveCTE R
+        WHERE P.Id = R.ProductId
+        ORDER BY R.Level DESC -- Order by Level in descending order to get the topmost indentation
+    ) AS Name,
+    L.Count,
+    P.Price,
+    ISNULL(R.AmountChildTotalCost, 0) AS AmountChildTotalCost,
+	ISNULL(R2.AmountChildCount, 0) AS AmountChildCount,
+    R.Level
+
+FROM
+    Links L
+    JOIN Product P ON L.ProductId = P.Id
+    LEFT JOIN (
+        SELECT
+            RootID, 
+            SUM(Price*Count) AS AmountChildTotalCost,
+            MAX(Level) AS Level
+        FROM
+            RecursiveCTE
+        GROUP BY
+            RootID
+    ) AS R ON P.Id = R.RootID
+	    LEFT JOIN (
+        SELECT
+            RootID2,
+            SUM(Count) AS AmountChildCount,
+            MAX(Level) AS Level
+        FROM
+            RecursiveCTE
+        GROUP BY
+            RootID2
+    ) AS R2 ON P.Id = R2.RootID2;
+-- Select and view the contents of the temporary table
+SELECT * FROM #TempTable;
+
+-- Drop the temporary table
+DROP TABLE #TempTable;
+-------------------------------------------------------------
+
+Create table #TempTable (
+UpProductId bigint,
+ProductId bigint,
+Name varchar(100),
+Count int,
+Price dec(18,2),
+AmountChild int
+,AmountChild2 dec(18,2)
+);
+
+
+;With RecursiveCTE as (  
+  select L.ProductId,  
+         L.Count,  
+		 P.Price,
+         L.UpProductId as RootID,
+		 L.ProductId as RootID2
+  from Links L
+    join Product P on L.ProductId = P.Id
+  
+  union all  
+  
+  select L.ProductId,  
+         L.Count,  
+		 P.Price,
+         R.RootID,
+		 R.RootID2
+  from Links L
+    join Product P on L.ProductId = P.Id
+    join RecursiveCTE R on L.UpProductId = R.ProductId  
+)  select * from RecursiveCTE
+INSERT INTO #TempTable (UpProductId, ProductId, Name, Count, Price, AmountChild, AmountChild2)
+SELECT DISTINCT L.UpProductId AS UpProductId,   
+       L.ProductId AS ProductId,   
+       P.Name,  	   
+       L.Count,  
+	   P.Price,
+       ISNULL(R.AmountChild, 0) AS AmountChild
+	   ,ISNULL(R2.AmountChild2, 0) AS AmountChild2 
+FROM Links L
+JOIN Product P ON L.ProductId = P.Id
+LEFT JOIN (  
+             SELECT RootID,  
+                    SUM(Count) AS AmountChild  
+             FROM RecursiveCTE
+             GROUP BY RootID  
+             ) AS R ON P.Id = R.RootID
+LEFT JOIN (
+			SELECT RootID2,  
+                  SUM(Price*Count) AS AmountChild2  
+             FROM RecursiveCTE
+             GROUP BY RootID2
+			) as R2	 ON P.Id = R2.RootID2;	 
+
+--Order by P.Id  
+--option (maxrecursion 0);
+
+SELECT * FROM  #TempTable;
+DROP TABLE #TempTable;
+
+
+
+
+
+
+
+
+
+
 
 
 -- 1 ----------------------------------------------------------
@@ -173,16 +519,14 @@ option (maxrecursion 0);
   select L.ProductId,  
          L.Count,
 		 P.Price,
-         L.ProductId as RootID, 
-		 0 as level
+         L.ProductId as RootID 
   from Links L
     join Product P on L.ProductId = P.Id
   union all  
   select L.ProductId,  
          L.Count, 
 		 P.Price,
-         C.RootID,
-		 C.level + 1
+         C.RootID
   from Links L
     join Product P on L.ProductId = P.Id
     join C   
@@ -201,10 +545,66 @@ from Links L
              select RootID, 
                     sum(Price*Count) as AmountIncludingChildren  
              from C  
-             group by RootID , Price
+             group by RootID
              ) as S  
     on P.Id = S.RootID  
 
 
 order by P.Id  
 option (maxrecursion 0);
+
+CREATE TABLE dbo.Hierarchy 
+(
+    ID INT NOT NULL PRIMARY KEY,
+    ParentID INT NULL,
+        CONSTRAINT [FK_parent] FOREIGN KEY ([ParentID]) REFERENCES dbo.Hierarchy([ID]),
+    hid HIERARCHYID,
+    Amount INT NOT null
+);
+
+INSERT INTO [dbo].[Hierarchy]
+        ( [ID], [ParentID], [Amount] )
+VALUES  
+    (1, NULL, 100 ),
+    (2, 1, 50),
+    (3, 1, 50),
+    (4, 2, 58),
+    (5, 2, 7),
+    (6, 3, 10),
+    (7, 3, 20)
+SELECT * FROM dbo.[Hierarchy] AS [h];
+
+WITH cte AS (
+    SELECT  [h].[ID] ,
+            [h].[ParentID] ,
+            CAST('/' + CAST(h.[ID] AS VARCHAR(10)) + '/' AS VARCHAR(MAX)) AS [h],
+            [h].[hid]
+    FROM    [dbo].[Hierarchy] AS [h]
+    WHERE   [h].[ParentID] IS NULL
+
+    UNION ALL
+
+    SELECT  [h].[ID] ,
+            [h].[ParentID] ,
+            CAST([c].[h] + CAST(h.[ID] AS VARCHAR(10)) + '/' AS VARCHAR(MAX)) AS [h],
+            [h].[hid]
+    FROM    [dbo].[Hierarchy] AS [h]
+    JOIN    [cte] AS [c]
+            ON [h].[ParentID] = [c].[ID]
+)
+UPDATE [h]
+SET hid = [cte].[h]
+FROM cte
+JOIN dbo.[Hierarchy] AS [h]
+    ON [h].[ID] = [cte].[ID];
+
+	Select * from [Hierarchy]
+
+	SELECT p.id, SUM([c].[Amount])
+FROM dbo.[Hierarchy] AS [p]
+JOIN [dbo].[Hierarchy] AS [c]
+    ON c.[hid].IsDescendantOf(p.[hid]) = 1
+GROUP BY [p].[ID];
+
+
+
